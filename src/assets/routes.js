8d9cdec3b31791a41f263a848533f4a837d6ea2b
Anestisv <80037315+Anestisv@users.noreply.github.com>
2023-02-21 03:44:59 +0100

    changed routes

--- src/assets/routes.js
+++ src/assets/routes.js
@@ -1,8 +1,8 @@
 export const allRoutes = { 
-    home: "/",
-    details: '/details',
-    rsvp: '/rsvp',
-    photos: '/photos',
-    contact: '/contact',
+    home: "/alex_and_sofia_wedding",
+    details: '/alex_and_sofia_wedding/details',
+    rsvp: '/alex_and_sofia_wedding/rsvp',
+    photos: '/alex_and_sofia_wedding/photos',
+    contact: '/alex_and_sofia_wedding/contact',
     noroute: '*'
 }
